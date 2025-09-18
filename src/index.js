11d255f781926cc88c50efdfeec4072128dc8805
keskiyo <makhilinmax@yandex.ru>
2025-09-18 15:22:34 +0700

    posts fix

--- src/index.js
+++ src/index.js
@@ -1,25 +1,26 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import App from "./App";
-import { Provider } from 'react-redux';
-import CssBaseline from "@mui/material/CssBaseline";
+import App from './App'
+import { Provider } from 'react-redux'
+import CssBaseline from '@mui/material/CssBaseline'
 
-import "./index.scss";
-import { ThemeProvider } from "@mui/material";
-import { theme } from "./theme";
+import './index.scss'
+import { ThemeProvider } from '@mui/material'
+import { theme } from './theme'
+import store from './redux/store'
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
-  <React.StrictMode>
-    <CssBaseline />
-    <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <Provider>
-          <App />
-        </Provider>
-      </BrowserRouter>     
-    </ThemeProvider>
-  </React.StrictMode>
-);
+	<>
+		<CssBaseline />
+		<ThemeProvider theme={theme}>
+			<BrowserRouter>
+				<Provider>
+					<App />
+				</Provider>
+			</BrowserRouter>
+		</ThemeProvider>
+	</>
+)
---
+++ src/redux/slices/post.js
@@ -0,0 +1,26 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import axios from '../../axios'
+
+export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
+	const { data } = await axios.get('/posts')
+	return data
+})
+
+const initialState = {
+	posts: {
+		items: [],
+		status: 'loading',
+	},
+	tags: {
+		items: [],
+		status: 'loading',
+	},
+}
+
+const postSlice = createSlice({
+	name: 'posts',
+	initialState,
+	reducers: {},
+})
+
+export const postsReducer = postSlice.reducer
